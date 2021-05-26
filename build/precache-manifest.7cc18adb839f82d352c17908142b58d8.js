97f21c7c772249d1595181db423d089902e06206
Joaquin Cicetti <joaquincicetti@gmail.com>
2021-05-25 21:32:10 -0300

    deploy

---
+++ build/precache-manifest.7cc18adb839f82d352c17908142b58d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e999ce64389587175e1e43cc5a46df82",
+    "url": "/aima/index.html"
+  },
+  {
+    "revision": "f4dfb2d194327672bf5c",
+    "url": "/aima/static/css/main.de223451.chunk.css"
+  },
+  {
+    "revision": "dd42f9578e32fffaf117",
+    "url": "/aima/static/js/2.0851d7aa.chunk.js"
+  },
+  {
+    "revision": "f4dfb2d194327672bf5c",
+    "url": "/aima/static/js/main.23e8afa5.chunk.js"
+  },
+  {
+    "revision": "31a64f5764a21248d8b1",
+    "url": "/aima/static/js/runtime-main.6487b3e5.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/aima/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2a05bbfed2bab7389937b8a7c05f9663",
+    "url": "/aima/static/media/aati-logo.2a05bbfe.png"
+  },
+  {
+    "revision": "e75f26b7cdc96a7bee9ce49fe5cce1d2",
+    "url": "/aima/static/media/aima.e75f26b7.png"
+  },
+  {
+    "revision": "059016579a39efdcef0ce1c3f3a2559e",
+    "url": "/aima/static/media/paula.05901657.png"
+  },
+  {
+    "revision": "71c29a43f3a5165c2c77b54f80f66eda",
+    "url": "/aima/static/media/rosario.71c29a43.png"
+  }
+]);
